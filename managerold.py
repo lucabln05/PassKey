bf8396a6c1a402c13b12caf70f1fe42400ef0d8e
lucabln05 <luca.bellon@icloud.com>
2023-03-12 20:12:50 +0100

    New Login, Create, Add UI with CustomTkinter

--- managerold.py
+++ managerold.py
@@ -1,6 +1,5 @@
 import tkinter as tk 
 from tkinter import ttk
-import customtkinter as ctk
 import webbrowser
 
 #local import
@@ -10,105 +9,95 @@ import handler.databasehandler as db
 class gui():
     def login_gui():
 
-        ctk.set_appearance_mode("system")
-        ctk.set_default_color_theme("blue")
-        
-        gui.login_gui.frame = ctk.CTk()
-        gui.login_gui.frame.geometry("400x400")
-        gui.login_gui.frame.title("PassKey")
+        # Create the main window
+        gui.login_gui.login = tk.Tk()
+        gui.login_gui.login.title("Login Page")
 
-        # Set the label
-        label = ctk.CTkLabel(gui.login_gui.frame,text="PassKey")
-        
-        label.pack(pady=20)
-        
-        # Create a frame
-        frame = ctk.CTkFrame(master=gui.login_gui.frame)
-        frame.pack(pady=20,padx=40,
-                fill='both',expand=True)
-        
-        # Set the label inside the frame
-        label = ctk.CTkLabel(master=frame,
-                            text='Vault Login')
-        label.pack(pady=12,padx=10)
-        
-        # Create the text box for taking
-        # username input from user
-        gui.login_gui.salt_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Salt")
-        gui.login_gui.salt_entry.pack(pady=12,padx=10)
-        
-        # Create a text box for taking 
-        # password input from user
-        gui.login_gui.password_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Password",
-                                show="*")
-        gui.login_gui.password_entry.pack(pady=12,padx=10)
-        
-        # Create a login button to login
-        button = ctk.CTkButton(master=frame,
-                            text='Login', command=command.login)
-        button.pack(pady=12,padx=10)
-        
-        # Create a create button to login
-        button = ctk.CTkButton(master=frame,
-                            text='Create', command=gui.create_gui)
-        button.pack(pady=12,padx=10)
-        
-        gui.login_gui.frame.mainloop()
+        # Set the window size and position
+        window_width = 400
+        window_height = 250
+        screen_width = gui.login_gui.login.winfo_screenwidth()
+        screen_height = gui.login_gui.login.winfo_screenheight()
+        x_coordinate = (screen_width/2) - (window_width/2)
+        y_coordinate = (screen_height/2) - (window_height/2)
+        gui.login_gui.login.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
+
+        # Create a frame to hold the login form
+        gui.login_gui.frame = tk.Frame(gui.login_gui.login, pady=20)
+        gui.login_gui.frame.pack()
+
+
+        # Create the login form labels and entries
+        username_label = tk.Label(gui.login_gui.frame, text="Salt:")
+        username_label.grid(row=0, column=0, padx=10, pady=5)
+
+        gui.login_gui.salt_entry = tk.Entry(gui.login_gui.frame, width=30)
+        gui.login_gui.salt_entry.grid(row=0, column=1, padx=10, pady=5)
+
+
+        password_label = tk.Label(gui.login_gui.frame, text="Password:")
+        password_label.grid(row=1, column=0, padx=10, pady=5)
+
+        gui.login_gui.password_entry = tk.Entry(gui.login_gui.frame, show="*", width=30)
+        gui.login_gui.password_entry.grid(row=1, column=1, padx=10, pady=5)
+
+        # Create the login button
+        create_button = tk.Button(gui.login_gui.frame, text="Create", bg="grey", fg="white", width=15, command=gui.create_gui)
+        create_button.grid(row=2, column=0, pady=10)
+
+        login_button = tk.Button(gui.login_gui.frame, text="Login", bg="#84d1eb", fg="white", width=15, command=command.login)
+        login_button.grid(row=2, column=1, pady=10)
+
+
+        # Add padding to the frame
+        gui.login_gui.frame.configure(pady=10)
+        gui.login_gui.login.mainloop()
 
     def create_gui():
-        ctk.set_appearance_mode("system")
-        ctk.set_default_color_theme("blue")
-        
-        gui.create_gui.frame = ctk.CTk()
-        gui.create_gui.frame.geometry("400x400")
-        gui.create_gui.frame.title("PassKey")
+       #create window for create the database with have input password and return the password and salt when the user click on create button or False if there is an error
+        gui.create_gui.create = tk.Tk()
+        gui.create_gui.create.title("Create Page")
+        gui.create_gui.frame = tk.Frame(gui.create_gui.create, pady=20)
+        gui.create_gui.frame.pack()
 
-        # Set the label
-        label = ctk.CTkLabel(gui.create_gui.frame,text="PassKey Create")
-        
-        label.pack(pady=20)
-        
-        # Create a frame
-        frame = ctk.CTkFrame(master=gui.create_gui.frame)
-        frame.pack(pady=20,padx=40,
-                fill='both',expand=True)
-        
-        # Set the label inside the frame
-        label = ctk.CTkLabel(master=frame,
-                            text='Create Vault')
-        label.pack(pady=12,padx=10)
-        
-        # Create the text box for taking
-        # username input from user
-        gui.create_gui.password_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Password", show="*")
-        gui.create_gui.password_entry.pack(pady=12,padx=10)
-        
-        # Create a text box for taking 
-        # password input from user
-        gui.create_gui.confirm_password_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Confirm Password",
-                                show="*")
-        gui.create_gui.confirm_password_entry.pack(pady=12,padx=10)
-        
-        # Create a create button to create vault
-        button = ctk.CTkButton(master=frame,
-                            text='Login', command=command.create)
-        button.pack(pady=12,padx=10)
+        # Set the window size and position
+        window_width = 400
+        window_height = 250
+        screen_width = gui.create_gui.create.winfo_screenwidth()
+        screen_height = gui.create_gui.create.winfo_screenheight()
+        x_coordinate = (screen_width/2) - (window_width/2)
+        y_coordinate = (screen_height/2) - (window_height/2)
+        gui.create_gui.create.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coordinate))
 
+
+        # Create the login form labels and entries
+        password_label = tk.Label(gui.create_gui.frame, text="Password:")
+        password_label.grid(row=0, column=0, padx=10, pady=5)
+
+        gui.create_gui.password_entry = tk.Entry(gui.create_gui.frame, show="*", width=30)
+        gui.create_gui.password_entry.grid(row=0, column=1, padx=10, pady=5)
+
+        password_label = tk.Label(gui.create_gui.frame, text="Confirm Password:")
+        password_label.grid(row=1, column=0, padx=10, pady=5)
+
+        gui.create_gui.confirm_password_entry = tk.Entry(gui.create_gui.frame, show="*", width=30)
+        gui.create_gui.confirm_password_entry.grid(row=1, column=1, padx=10, pady=5)
+
+        # Create the login button
+        create_button = tk.Button(gui.create_gui.frame, text="Create", bg="#84d1eb", fg="white", width=15, command=command.create)
+        create_button.grid(row=2, column=1, pady=10)
+
+            
+
+        # Add padding to the frame
+        gui.create_gui.frame.configure(pady=10)
+        gui.create_gui.create.mainloop()
         
-        gui.create_gui.frame.mainloop()
-        
-    def safe_gui():     
+    def safe_gui():
         # Create the main window
-        ctk.set_appearance_mode("Light")
-        ctk.set_default_color_theme("blue")
-        
-        gui.safe_gui.safe = ctk.CTk()
-        gui.safe_gui.safe.title("PassKey")
-        gui.safe_gui.safe.geometry("800x500")
+        gui.safe_gui.safe = tk.Tk()
+        gui.safe_gui.safe.title("Safe")
+
         # Set the window size and position
         window_width = 800
         window_height = 500
@@ -170,7 +159,7 @@ class gui():
                 gui.safe_gui.safe.clipboard_append(password)
             
             def on_one_click(event):
-                
+
                 # diplay the content of the selected row on the right side with a label
                 item = tree.selection()[0]
                 service_name = tree.item(item, "values")[0]
@@ -259,68 +248,55 @@ class gui():
         gui.safe_gui.safe.mainloop()
 
     def add_gui():
-        gui.safe_gui.safe.destroy()
-        ctk.set_appearance_mode("system")
-        ctk.set_default_color_theme("blue")
-        
-        gui.add_gui.frame = ctk.CTk()
-        gui.add_gui.frame.geometry("400x600")
-        gui.add_gui.frame.title("PassKey")
+                gui.safe_gui.safe.destroy()
+                add = tk.Tk()
+                add.title("Add")
+                add.geometry("300x300")
+                # create the labels and entries
+                service_name_label = tk.Label(add, text="Service Name:")
+                service_name_label.grid(row=0, column=0, padx=10, pady=5)
+                service_name_entry = tk.Entry(add, width=30)
+                service_name_entry.grid(row=0, column=1, padx=10, pady=5)
+                username_label = tk.Label(add, text="Username:")
+                username_label.grid(row=1, column=0, padx=10, pady=5)
+                username_entry = tk.Entry(add, width=30)
+                username_entry.grid(row=1, column=1, padx=10, pady=5)
+                password_label = tk.Label(add, text="Password:")
+                password_label.grid(row=2, column=0, padx=10, pady=5)
+                password_entry = tk.Entry(add, width=30)
+                password_entry.grid(row=2, column=1, padx=10, pady=5)
+                url_label = tk.Label(add, text="URL:")
+                url_label.grid(row=3, column=0, padx=10, pady=5)
+                url_entry = tk.Entry(add, width=30)
+                url_entry.grid(row=3, column=1, padx=10, pady=5)
+                notes_label = tk.Label(add, text="Notes:")
+                notes_label.grid(row=4, column=0, padx=10, pady=5)
+                notes_entry = tk.Entry(add, width=30)
+                notes_entry.grid(row=4, column=1, padx=10, pady=5)
+                # create the add button
+                def addhandler():
+                    objectname = service_name_entry.get()
+                    username = username_entry.get()
+                    password = password_entry.get()
+                    url = url_entry.get()
+                    notes = notes_entry.get()
 
-        # Set the label
-        label = ctk.CTkLabel(gui.add_gui.frame,text="PassKey Add")
-        
-        label.pack(pady=20)
-        
-        # Create a frame
-        frame = ctk.CTkFrame(master=gui.add_gui.frame)
-        frame.pack(pady=20,padx=40,
-                fill='both',expand=True)
-        
-        # Set the label inside the frame
-        label = ctk.CTkLabel(master=frame,
-                                text='Add Service')
-        label.pack(pady=12,padx=10)
-        service_name_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Service Name")
-        service_name_entry.pack(pady=12,padx=10)
-        username_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Username")
-        username_entry.pack(pady=12,padx=10)
-        password_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Password")
-        password_entry.pack(pady=12,padx=10)
-        url_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="URL")
-        url_entry.pack(pady=12,padx=10)
-        notes_entry= ctk.CTkEntry(master=frame,
-                                placeholder_text="Notes")
-        notes_entry.pack(pady=12,padx=10)
-        
+                    content = [objectname, username, password, url, notes]
 
-        def addhandler():
-                objectname = service_name_entry.get()
-                username = username_entry.get()
-                password = password_entry.get()
-                url = url_entry.get()
-                notes = notes_entry.get()
+                    if db.encrypt_object(content, safe_password, safe_salt) == True:
+                        print("Object encrypted and added to the database")
+                        add.destroy()
+                        gui.safe_gui()
+                    else:
+                        print("Error adding object to the database")
 
-                content = [objectname, username, password, url, notes]
+                add_button = tk.Button(add, text="Add", bg="blue", fg="white", width=15, command=addhandler)
+                add_button.grid(row=5, column=1, pady=10)
 
-                if db.encrypt_object(content, safe_password, safe_salt) == True:
-                    print("Object encrypted and added to the database")
-                    gui.add_gui.frame.destroy()
-                    gui.safe_gui()
-                else:
-                    print("Error adding object to the database")
-        
-        # Create a create button to create vault
-        button = ctk.CTkButton(master=frame,
-                                text='Add', command=addhandler)
-        button.pack(pady=12,padx=10)
+                
 
-        
-        gui.add_gui.frame.mainloop()
+
+                add.mainloop()
 
 class command():
     def login():
@@ -331,8 +307,12 @@ class command():
         safe_password = gui.login_gui.password_entry.get()
         db.check.check_password(safe_password, safe_salt)
         if db.check.check_password(safe_password, safe_salt) == True:
-            gui.login_gui.frame.destroy()
+            gui.login_gui.login.destroy()
             gui.safe_gui()
+        else:
+            tk.Label(gui.login_gui.frame, text="Wrong password or salt").grid(row=3, column=0, padx=0, pady=0)
+            gui.login_gui.salt_entry.delete(0, tk.END)
+            gui.login_gui.password_entry.delete(0, tk.END)
     
     def create():
             
@@ -344,19 +324,18 @@ class command():
                         for widget in gui.create_gui.frame.winfo_children():
                             widget.destroy()
 
-                        labelueb = ctk.CTkLabel(master=gui.create_gui.frame,
-                                        text=f'Vault created successfully!')
-                        labelueb.pack(pady=12,padx=10)
+                        tk.Label(gui.create_gui.frame, text="Please save following Password and Salt.").grid(row=1, column=0, padx=0, pady=0)
 
-                        label1 = ctk.CTkLabel(master=gui.create_gui.frame,
-                                        text=f'''
-Salt:  {data[1]} 
-Password:  {data[0]}''')
-                        label1.pack(pady=12,padx=10)
+                        tk.Label(gui.create_gui.frame, text=f"Salt: {data[1]}").grid(row=2, column=0, padx=0, pady=0)
+                        tk.Label(gui.create_gui.frame, text=f"Password: {data[0]}").grid(row=3, column=0, padx=0, pady=0)
 
-                        label2 = ctk.CTkLabel(master=gui.create_gui.frame,
-                                        text=f'Please save this information in a safe place.')
-                        label2.pack(pady=12,padx=10)
+                        tk.Label(gui.create_gui.frame, text="IF YOU FORGOT YOUR PASSWORD OR SALT OF YOUR DATABASE,", bg="red").grid(row=4, column=0, padx=0, pady=0)
+                        tk.Label(gui.create_gui.frame, text="YOU WILL NOT BE ABLE TO ACCESS YOUR PASSWORDS.", bg="red").grid(row=5, column=0, padx=0, pady=0)
+                        tk.Label(gui.create_gui.frame, text="PLEASE SAVE YOUR PASSWORD AND SALT IN A SAFE PLACE.", bg="red").grid(row=5, column=0, padx=0, pady=0)
+
+                        tk.Label(gui.create_gui.frame, text="Database Created", bg="green", width=20).grid(row=7, column=0, padx=0, pady=0)
+                        tk.Label(gui.create_gui.frame, text="Salt and Password was copied in clipboard,", bg="green", width=20).grid(row=8, column=0, padx=0, pady=0)
+                        tk.Label(gui.create_gui.frame, text="PLEASE SAVE IT.", bg="green", width=20).grid(row=9, column=0, padx=0, pady=0)
                         # copies the password and salt to the clipboard
                         gui.create_gui.frame.clipboard_clear()
                         gui.create_gui.frame.clipboard_append(f"Salt: {data[1]} Password: {data[0]}")
@@ -369,22 +348,17 @@ Password:  {data[0]}''')
                         # clear row 3
                         for widget in gui.create_gui.frame.grid_slaves(row=3):
                             widget.destroy()
-                        label1 = ctk.CTkLabel(master=gui.create_gui.frame,
-                            text=f'Password to short, or Vault already exists.')
-                        label1.pack(pady=12,padx=10)
+                        tk.Label(gui.create_gui.frame, text="Password is to short.").grid(row=3, column=1, padx=0, pady=0)
 
                 except:
                                             # clear row 3
                     for widget in gui.create_gui.frame.grid_slaves(row=3):
                             widget.destroy()
-                    label1 = ctk.CTkLabel(master=gui.create_gui.frame,
-                            text=f'Error creating vault.')
-                    label1.pack(pady=12,padx=10)
+                    tk.Label(gui.create_gui.frame, text="Error").grid(row=3, column=0, padx=1, pady=0)
             else:
                                         # clear row 3
                 for widget in gui.create_gui.frame.grid_slaves(row=3):
                             widget.destroy()
-                label1 = ctk.CTkLabel(master=gui.create_gui.frame,
-                            text=f'Passwords do not match.')
-                label1.pack(pady=12,padx=10)
+                tk.Label(gui.create_gui.frame, text="Password and Confirm Password does not match.").grid(row=3, column=1, padx=0, pady=0)
+
 gui.login_gui()
